8f6f7a474340bb91b5070dc332ca21c63d470572
Rsls12 <rlucensama@gmail.com>
2025-05-26 19:02:27 -0500

    ccommit

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,7 +1,6 @@
 import { createContext, useContext, useState } from 'react';
 
 const CartContext = createContext();
-// Hook personalizado para usar el contexto
 export const useCart = () => useContext(CartContext);
 
 const CartProvider = ({ children }) => {
@@ -9,7 +8,7 @@ const CartProvider = ({ children }) => {
   const [direccion, setDireccion] = useState(null);
   const limpiarDireccion = () => setDireccion(null);
 
-  // Agregar producto (si ya existe, aumenta cantidad)
+// Agregar producto
   const agregarAlCarrito = (producto) => {
     setCarrito((prev) => {
       const existe = prev.find((item) => item.id === producto.id);
@@ -25,12 +24,12 @@ const CartProvider = ({ children }) => {
     });
   };
 
-  // Eliminar producto por ID
+//Eliminar producto por ID
   const eliminarProducto = (id) => {
     setCarrito((prev) => prev.filter((item) => item.id !== id));
   };
 
-  // Cambiar cantidad manualmente
+//Cambiar cantidad manualmente
   const actualizarCantidad = (id, nuevaCantidad) => {
     setCarrito((prev) =>
       prev.map((item) =>
@@ -39,12 +38,12 @@ const CartProvider = ({ children }) => {
     );
   };
 
-  // Vaciar todo el carrito
+//Vaciar todo el carrito
   const vaciarCarrito = () => {
     setCarrito([]);
   };
 
-  // Calcular total
+//Calcular total
   const obtenerTotal = () => {
     return carrito.reduce((acc, item) => acc + item.precio * item.cantidad, 0);
   };
