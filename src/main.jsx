2993bcdaf97c9b1859928fa899516b334334a91c
licetvaleria <20222052@aloe.ulima.edu.pe>
2025-05-28 21:24:12 -0500

    Agregando Categorias

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import Fin from './routes/Fin.jsx';
 import Catalogo from './routes/Catalogo.jsx';
 import Resultados from './resultados.jsx';
 import Dashboardpage from './routes/Dashboard.jsx';
-
+import Cate from './routes/ListaCate.jsx';
 const router = createBrowserRouter([
   { path: '/', element: <App /> },
   { path: '/cart', element: <Cart /> },
@@ -20,6 +20,7 @@ const router = createBrowserRouter([
   { path: '/catalogo', element: <Catalogo /> },
   { path: '/resultados', element: <Resultados /> },
   { path: '/dashboard', element: <Dashboardpage /> },
+  { path: '/Categorias', element: <Cate /> },
 ])
 
 createRoot(document.getElementById('root')).render(
